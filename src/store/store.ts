42509db5a5d97903a32791b3a43528c3fd1386ce
ruslankriklivyy <miminimalizm@gmail.com>
2021-06-15 18:35:01 +0300

    Отрефакторил стор

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,29 +1,39 @@
 import { combine, createEvent, createStore } from 'effector';
 import { ISubsColor, ISubsItem } from '../interfaces/interfaces';
 
-// Events
-export const setIsEditSub = createEvent<boolean>();
+// Events subsItems
 export const addToSubsItems = createEvent<ISubsItem>();
 export const removeItemFromSubs = createEvent<string>();
 export const editSubItem = createEvent<ISubsItem>();
+
+// Events visibleModals
 export const setIsVisibleModal = createEvent<boolean>();
 export const setIsVisiblePicker = createEvent<boolean>();
+
+// Events user
 export const setUserProfit = createEvent<number>();
 
+// Events AddModal inputs values
 export const setSubColor = createEvent<ISubsColor>();
 export const setSubName = createEvent<string>();
 export const setSubPrice = createEvent<number>();
 export const setSubPayment = createEvent<number>();
-export const setEditSubId = createEvent<string>();
 
-/// Stores
+// Events edit mode
+export const setEditSubId = createEvent<string>();
+export const setIsEditSub = createEvent<boolean>();
+
+// Stores AddModal inputs values
 export const $subColor = createStore({ r: 0, g: 0, b: 0 }).on(setSubColor, (state, color) => color);
 export const $subName = createStore('').on(setSubName, (state, name) => name);
 export const $subPrice = createStore(0).on(setSubPrice, (state, price) => price);
 export const $subPayment = createStore(0).on(setSubPayment, (state, payment) => payment);
-export const $editSubId = createStore('').on(setEditSubId, (state, subId) => subId);
 
+// Stores edit mode
+export const $editSubId = createStore('').on(setEditSubId, (state, subId) => subId);
 export const $isEditSub = createStore(false).on(setIsEditSub, (state, visible) => visible);
+
+// Stores visibleModals
 export const $isVisibleModal = createStore(false).on(
   setIsVisibleModal,
   (state, visible) => visible,
@@ -33,6 +43,7 @@ export const $isVisiblePicker = createStore(false).on(
   (state, visible) => visible,
 );
 
+// Stores subsItems
 export const $subsItems = createStore([
   { id: '1', name: 'Netflix', price: 14.5, payment: 1, color: [66, 153, 118] },
 ])
@@ -47,8 +58,11 @@ export const $subsItems = createStore([
     });
   });
 
+// Stores user
 export const $userProfit = createStore(120).on(setUserProfit, (state, profit) => profit);
 export const $userTotalSubs = createStore(0);
+
+// Stores total sum
 export const $totalSumSubs = combine($subsItems, $userTotalSubs, (subs, total) => {
   const totalSubs = subs.map((item) => item.price);
   const totalSum = totalSubs.reduce((acc, num) => (acc += num));
